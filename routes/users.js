cc89498eee6a24383f0e58f25b2df5d573a62fa5
meni <meni3311il@gmail.com>
2024-05-08 10:03:59 +0300

    meni2

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async(req,res) => {
-  res.json({msg:"Users work111"});
+  res.json({msg:"Users work222meni"});
 })
 
 module.exports = router;
