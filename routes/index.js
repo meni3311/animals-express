582ecacd2911656d32d0c4f296ed5ff0fb427f2c
meni <meni3311il@gmail.com>
2024-05-08 09:54:27 +0300

    change

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async(req,res) => {
-  res.json({msg:"Express home endpoint work"});
+  res.json({msg:"Express home endpoint work!33333"});
 })
 
 module.exports = router;
